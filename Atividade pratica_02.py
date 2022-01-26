3c5b2c3243c5969633d4c486aaad71a33e627df6
tatijper <95759381+tatijper@users.noreply.github.com>
2022-01-26 12:25:50 -0300

    Add files via upload

---
+++ Atividade pratica_02.py
@@ -0,0 +1,7 @@
+nome=input('Digite o nome: ')
+#substituição de strings, fazendo interação das anteriores#
+nome1 = nome.replace('a', '@')
+nome2 = nome1.replace('e', '&')
+nome3 = nome2.replace('i', '!')
+nome4 = nome3.replace('u', '*')
+print(nome4)
